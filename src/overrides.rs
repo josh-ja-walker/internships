d965fdf04f6479b903f24cb8a5148f3fd59f630c
Josh Walker <josh.walker1@outlook.com>
2024-11-16 20:51:22 +0000

    Ensured no override modifications are made if the shifts before/after cannot be found

--- src/overrides.rs
+++ src/overrides.rs
@@ -16,28 +16,26 @@ pub fn apply_overrides(shifts: &mut Vec<Shift>, overrides: Vec<Shift>) {
 /* Apply single override */
 fn apply_override(override_shift: Shift, shifts: &mut Vec<Shift>) {
     let prev_shift: Option<usize> = helpers::find_shift_index(override_shift.start_at(), &shifts);
-    let post_shift: Option<usize> = helpers::find_shift_index(override_shift.end_at(), &shifts);
+    let mut post_shift: Option<usize> = helpers::find_shift_index(override_shift.end_at(), &shifts);
     
-    /* Helper function to unpack and clamp index of shift to avoid index out of range errors */
-    let clamp_idx = |idx: Option<usize>| (idx.unwrap_or(0)).clamp(0, shifts.len() - 1);
-    
-    /* Clamp prev and post shift indices */
-    let clamped_prev: usize = clamp_idx(prev_shift); 
-    let mut clamped_post: usize = clamp_idx(post_shift);
-
-    /* If override is contained within a single shift */
-    if clamped_prev == clamped_post {
-        /* Duplicate the shift to create post shift */
-        clamped_post = clamped_prev + 1;
-        shifts.insert(clamped_post, shifts[clamped_prev].clone());
+    // Do not modify existing shifts if the shift index cannot be found
+    if let (Some(prev), Some(post)) = (prev_shift, post_shift) {
+        /* Helper function to unpack and clamp index of shift to avoid index out of range errors */
+        
+        /* If override is contained within a single shift */
+        if prev == post {
+            /* Duplicate the shift to create post shift */
+            post_shift = Some(prev + 1);
+            shifts.insert(post, shifts[prev].clone());
+        } 
     }
-    
+
     /* Modify existing shifts */
-    shifts[clamped_prev].set_end_at(override_shift.start_at()); /* End previous shift earlier */
-    shifts[clamped_post].set_start_at(override_shift.end_at()); /* Start next shift later */
+    prev_shift.map(|i| shifts[i].set_end_at(override_shift.start_at())); /* End previous shift earlier */
+    post_shift.map(|j| shifts[j].set_start_at(override_shift.end_at())); /* Start next shift later */
 
     /* Remove all entirely overridden shifts */
-    shifts.drain((clamped_prev + 1)..clamped_post);
+    shifts.drain((prev_shift.unwrap_or(0) + 1)..post_shift.unwrap_or(shifts.len() - 1));
 
     /* Insert override - ensure override is inserted after the previous shift or at 0 if prev is None */
     shifts.insert(prev_shift.map_or(0, |i| i + 1), override_shift);
